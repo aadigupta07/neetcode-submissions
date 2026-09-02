1bc6707621c49b8ac9886151aaac7ee84f4633c7
aadigupta07 <86427718+aadigupta07@users.noreply.github.com>
2026-09-01 22:53:56 -0400

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        combined = []
+        for i in range(len(position)):
+            combined.append((position[i], speed[i]))
+        combined.sort()
+        
+        stack = []
+        for i in range(len(position)):
+            time = (target-combined[i][0])/combined[i][1]
+            if not stack or stack[-1] > time:
+                stack.append(time)
+            else:
+                while stack and stack[-1] <= time:
+                    stack.pop()
+                stack.append(time)
+        
+        return len(stack)
+            
+                
+            
+
+                
+            
